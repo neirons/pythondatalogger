3fb2c1c1d259a145510c133c99316f416edd36ec
shixiaoping79 <shixiaoping79@5c1c4db6-e7b1-408c-73c2-e61316242a6a>
2010-05-01 14:01:52 +0000

    shixiaoping:can save bmp file but there is a bug

--- vc/DataLogger/resource.h
+++ vc/DataLogger/resource.h
@@ -6,13 +6,14 @@
 #define IDD_ABOUTBOX                    100
 #define IDS_ABOUTBOX                    101
 #define IDD_DATALOGGER_DIALOG           102
+#define IDS_WINDOWS_BITMAP_FILES        102
+#define IDS_FILE_CREATE_ERROR_MESSAGE   103
 #define IDR_MAINFRAME                   128
 #define IDC_STATIC_GRAPH                1000
 #define IDC_BUTTON_SAVE                 1001
 #define IDC_BUTTON_PRINT                1002
 #define IDC_BATTERY                     1003
 #define IDC_BUTTON_CLEAR                1004
-#define IDC_EDIT1                       1005
 
 // Next default values for new objects
 // 
