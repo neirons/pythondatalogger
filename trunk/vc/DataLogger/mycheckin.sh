d37c58ed740ce8a71e3bbc7668db275bf5e5020d
shixiaoping79 <shixiaoping79@5c1c4db6-e7b1-408c-73c2-e61316242a6a>
2010-05-07 12:44:51 +0000

    add st check in the shell

--- trunk/vc/DataLogger/mycheckin.sh
+++ trunk/vc/DataLogger/mycheckin.sh
@@ -6,17 +6,21 @@ then
     echo "Usage: ci comment"
     exit -1
 fi
-output=`svn up`
-echo $output
-version=${output##*"At revision "}
-version=${version%%"."}
-echo $version
-version=$((version+1))
-echo $version
-re_str="s/svnr\:[0-9]\{1,\}/svnr\:`echo ${version}`/g"
-echo $re_str
-sed -e $re_str DataLogger.rc > DataLogger.rc.back
-mv DataLogger.rc.back DataLogger.rc
-echo "$1"
-svn ci -m "$1"
+output=`svn st|grep -v ^?`
 
+if [[ $output != "" ]];
+then
+	output=`svn up`
+	echo $output
+	version=${output##*"At revision "}
+	version=${version%%"."}
+	echo $version
+	version=$((version+1))
+	echo $version
+	re_str="s/svnr\:[0-9]\{1,\}/svnr\:`echo ${version}`/g"
+	echo $re_str
+	sed -e $re_str DataLogger.rc > DataLogger.rc.back
+	mv DataLogger.rc.back DataLogger.rc
+	echo "$1"
+	svn ci -m "$1"
+fi
