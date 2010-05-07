96e02fa9cc40bf6df8278b1359cb0f1f1bb9a43a
shixiaoping79 <shixiaoping79@5c1c4db6-e7b1-408c-73c2-e61316242a6a>
2010-05-07 12:37:29 +0000

    add the version in shell

--- trunk/vc/DataLogger/mycheckin.sh
+++ trunk/vc/DataLogger/mycheckin.sh
@@ -11,8 +11,12 @@ echo $output
 version=${output##*"At revision "}
 version=${version%%"."}
 echo $version
+version=$((version+1))
+echo $version
 re_str="s/svnr\:[0-9]\{1,\}/svnr\:`echo ${version}`/g"
 echo $re_str
-sed -e $re_str DataLogger.rc > DataLogger.rc
+sed -e $re_str DataLogger.rc > DataLogger.rc.back
+mv DataLogger.rc.back DataLogger.rc
+echo "$1"
 svn ci -m "$1"
 
