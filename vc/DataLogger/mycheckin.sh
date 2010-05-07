fad48747feb1b7d403810ed25546d7c15c442885
shixiaoping79 <shixiaoping79@5c1c4db6-e7b1-408c-73c2-e61316242a6a>
2010-05-07 12:51:35 +0000

    modify the shell

--- vc/DataLogger/mycheckin.sh
+++ vc/DataLogger/mycheckin.sh
@@ -7,18 +7,14 @@ then
     exit -1
 fi
 output=`svn st|grep -v ^?`
-echo $output
-if [[ $output != "" ]];
+if [[ $output != "" ]]
 then
 	output=`svn up`
 	echo $output
 	version=${output##*"At revision "}
 	version=${version%%"."}
-	echo $version
 	version=$((version+1))
-	echo $version
 	re_str="s/svnr\:[0-9]\{1,\}/svnr\:`echo ${version}`/g"
-	echo $re_str
 	sed -e $re_str DataLogger.rc > DataLogger.rc.back
 	mv DataLogger.rc.back DataLogger.rc
 	svn ci -m "$1"
